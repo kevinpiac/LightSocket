9d6325bbf0a9da0066d7f18fbace1d4f5faa9c73
Kevin Piacentini <kevinpiac@gmail.com>
2016-12-22 10:41:05 +0100

    Initial Commit

---
+++ LightSocket/LightSocket.h
@@ -0,0 +1,13 @@
+//
+//  LightSocket.h
+//  LightSocket
+//
+//  Created by Kevin Piacentini on 22/12/2016.
+//  Copyright © 2016 Kevin Piacentini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LightSocket : NSObject
+
+@end
---
+++ LightSocket/LightSocket.m
@@ -0,0 +1,13 @@
+//
+//  LightSocket.m
+//  LightSocket
+//
+//  Created by Kevin Piacentini on 22/12/2016.
+//  Copyright © 2016 Kevin Piacentini. All rights reserved.
+//
+
+#import "LightSocket.h"
+
+@implementation LightSocket
+
+@end
