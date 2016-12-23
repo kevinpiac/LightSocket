2b57602cd80bba96eac6b15bcb9a294cd5b01773
Kevin Piacentini <kevinpiac@gmail.com>
2016-12-23 14:41:42 +0100

    Simple socket server

--- LightSocket/LightSocket.h
+++ LightSocket/LightSocket.h
@@ -7,7 +7,25 @@
 //
 
 #import <Foundation/Foundation.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define BUFF_SIZE 1024
+#define MAX_CONN 10
+#define END_OF_MSG '\n'
 
 @interface LightSocket : NSObject
+
+@property (nonatomic) int                   port;
+@property (nonatomic) int                   sockfd;
+@property (nonatomic) BOOL                  listening;
+@property (nonatomic) void                  *buffer;
+@property (nonatomic) int                   buff_size;
+
+- (id)initWithPort:(int)port;
+- (id)initWithFileDescriptor:(int)fd;
+- (void)readMessage;
+- (void)sendMessage:(NSString *)message;
+- (BOOL)startServer;
 
 @end
--- LightSocket/LightSocket.m
+++ LightSocket/LightSocket.m
@@ -10,4 +10,94 @@
 
 @implementation LightSocket
 
+- (id)initWithPort:(int)port
+{
+    if ((self = [super init]))
+    {
+        _port = port;
+        _sockfd = 0;
+        _listening = NO;
+    }
+    return self;
+}
+
+- (id)initWithFileDescriptor:(int)fd
+{
+    if ((self = [super init]))
+    {
+        _sockfd = fd;
+        _buff_size = BUFF_SIZE;
+    }
+    return self;
+}
+
+- (void)onMessageAction:(NSString *)message
+{
+    [self sendMessage:message];
+}
+
+- (void)readMessage
+{
+    BOOL                continueReading;
+    FILE                *stream;
+
+    continueReading = YES;
+    stream = fdopen(_sockfd, "r");
+    while ((getdelim((char **)(&_buffer), (size_t *)(&_buff_size), END_OF_MSG, stream)) != -1)
+    {
+        [self onMessageAction:[NSString stringWithUTF8String:(const char *)_buffer]];
+        bzero(_buffer, _buff_size);
+    }
+    [self close];
+}
+
+- (void)sendMessage:(NSString *)message
+{
+    const char *msg;
+
+    msg = [message UTF8String];
+    write(_sockfd, msg, strlen((const char *)msg));
+}
+
+- (BOOL)close {
+    if (_sockfd > 0 && close(_sockfd) < 0)
+        return NO;
+    _sockfd = 0;
+    return YES;
+}
+
+- (void)dealloc {
+    [self close];
+    free(_buffer);
+}
+
+- (BOOL)startServer
+{
+    struct sockaddr_in serv_addr;
+    
+    _sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(_port);
+    
+    bind(_sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    
+    if (listen(_sockfd, MAX_CONN) == -1)
+        return NO;
+    _listening = YES;
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        while (_listening) {
+            __block int connfd = accept(_sockfd, (struct sockaddr*)NULL, NULL);
+            if (connfd != -1)
+            {
+                LightSocket *socket = [[LightSocket alloc] initWithFileDescriptor:connfd];
+                [socket readMessage];
+                [socket close];
+            }
+        }
+    });
+    return YES;
+}
 @end
